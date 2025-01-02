efb7227dc69e59cd28e029ac9c69aa91e3bdbd7e
Lucas <lucasoliveirars22@gmail.com>
2025-01-02 16:21:26 -0300

    product controller

---
+++ ecommerce/src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -0,0 +1,72 @@
+package com.example.ecommerce.controller;
+
+import com.example.ecommerce.domain.dto.product.ProductRequestDTO;
+import com.example.ecommerce.domain.dto.product.ProductRequestPatchDTO;
+import com.example.ecommerce.domain.dto.product.ProductRequestPutDTO;
+import com.example.ecommerce.domain.dto.product.ProductResponseDTO;
+import com.example.ecommerce.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @Operation(summary = "Listar todos os produtos")
+    @GetMapping("/")
+    public ResponseEntity<Page<ProductResponseDTO>> list(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<ProductResponseDTO> productPage = productService.list(pageable);
+
+        return ResponseEntity.ok(productPage);
+    }
+
+    @Operation(summary = "Listar um produto")
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getById(id));
+    }
+
+    @Operation(summary = "Criar um produto")
+    @PostMapping("/")
+    public ResponseEntity<ProductResponseDTO> create(@Valid @RequestBody ProductRequestDTO body) {
+        return ResponseEntity.ok(productService.save(body));
+    }
+
+    @Operation(summary = "Atualizar um produto")
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> update(
+            @PathVariable Long id,
+            @Valid @RequestBody ProductRequestPutDTO body
+    ) {
+        return ResponseEntity.ok(productService.update(id, body));
+    }
+
+    @Operation(summary = "Deletar um produto")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.delete((id));
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "Atualizar estoque do produto")
+    @PatchMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> updateProductInventory(
+            @PathVariable Long id,
+            @RequestBody ProductRequestPatchDTO body
+    ) {
+        return ResponseEntity.ok(productService.updateInventory(id, body));
+    }
+}
